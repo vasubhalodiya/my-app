62bf12ff12dbfeb8a6aba8ed9f11f5f75763463e
vasubhalodiya <vasubhalodiya30@gmail.com>
2024-08-08 17:46:40 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css'
+import Navbar from './components/navbar'
 
 function App() {
-
     return (
         <>
-            <div className="nav">
-                hello
-            </div>
+            <Navbar />
         </>
     )
 }
